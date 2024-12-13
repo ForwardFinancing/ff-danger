b234f9236beba7eb525af812764a0434bfe3c2af
Tony Schaffert <tschaffert@forwardfinancing.com>
2024-12-13 08:05:37 -0500

    Add Private Key Rule

---
+++ lib/forward_financing_danger/rules/private_key_rule.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative './rule'
+
+module ForwardFinancingDanger
+  class PrivateKeyRule < ForwardFinancingDanger::Rule
+    def check!
+        changed_files.each do |path|
+          lines_from_file(path).each do |line|
+            if line.content.match(/PRIVATE KEY/)
+                failure(private_key_error_message, file: path, line: line.number)
+            end
+          end
+        end
+      end
+
+    private
+
+    def private_key_error_message
+      'This is a NO NO. Do not commit private keys to the repository.'
+    end
+  end
+end
